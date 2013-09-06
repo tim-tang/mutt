a42cfc2c3e8952ef7c909c2b0aceb7444d851296
tim.tang <tang.jilong@139.com>
2013-09-06 15:56:57 +0800

    [mutt] - Update install script for mail folder bootstrap.

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cp -rf ./.mutt $HOME/
+mkdir -p $HOME/.mutt/cache/headers
+mkdir $HOME/.mutt/cache/bodies
+touch $HOME/.mutt/certificates
 
 cp -f ./.muttrc $HOME/.muttrc
-
 cp -f ./.urlview $HOME/.urlview
-
 echo "Install mutt configuration finished."
