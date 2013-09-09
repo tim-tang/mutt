7b9c6fabea979ef3a06e80d4b2192fa030b16e2b
tim.tang <tang.jilong@139.com>
2013-09-09 08:46:06 +0800

    [mutt] - Refactor code to use symbol link.

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/zsh
 
 cp -rf ./.mutt $HOME/
 mkdir -p $HOME/.mutt/cache/headers
 mkdir $HOME/.mutt/cache/bodies
 touch $HOME/.mutt/certificates
 
-cp -f ./.muttrc $HOME/.muttrc
-cp -f ./.urlview $HOME/.urlview
-echo "Install mutt configuration finished."
+for link_file ($PWD/*.symlink); do
+    FILENAME=${link_file##*/}
+    ln -s -i -v $link_file $HOME/.${FILENAME%.*}
+done
+echo "Re-configure Mutt Success."
